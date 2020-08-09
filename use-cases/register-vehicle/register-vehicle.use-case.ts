8c89a23bea31b8333a61a06865547557b421c039
Tolis <apostolos94@gmail.com>
2020-08-09 16:49:45 +0300

    fix eslint command

--- use-cases/register-vehicle/register-vehicle.use-case.ts
+++ use-cases/register-vehicle/register-vehicle.use-case.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export class RegisterVehicleUseCase implements UseCaseExecutor<any, any> {
-  execute(test: any): any {
+  execute(test: string): any {
     return "";
   }
 }
